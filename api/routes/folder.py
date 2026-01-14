1961c26471b4c562504b218f983166b8b8da4402
rusXL <ruslan.melnyk.x@gmail.com>
2026-01-14 12:30:03 +0100

    feat: filters

--- api/routes/folder.py
+++ api/routes/folder.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter, HTTPException
-from models import Folder, FolderBase, NoteBase
+from typing import Optional
+from models import Folder, FolderBase, NoteBase, Priority
+
 from database import get_db
 
 router = APIRouter()
 
 
 @router.get("/folder/{folder_id}", response_model=Folder)
-def get_folder(folder_id: int):
+def get_folder(
+    folder_id: int,
+    with_image: bool = False,
+    with_caption: bool = False,
+    priority: Optional[Priority] = None,
+):
+
     """Get folder name, color, subfolders and notes."""
     with get_db() as conn:
         with conn.cursor(dictionary=True) as cur:
@@ -28,10 +36,26 @@ def get_folder(folder_id: int):
             subfolders = cur.fetchall()
 
             # get notes
-            cur.execute(
-                "SELECT id, name FROM note WHERE folder_id = %s", (folder_id,)
-            )  # no need for content
+            query = "SELECT n.id, n.name FROM note n"
+            params = [folder_id]
+            joins = []
+            conditions = ["n.folder_id = %s"]
+
+            if with_image or with_caption:
+                joins.append("JOIN image i ON n.id = i.note_id")
+                if with_caption:
+                    conditions.append("i.caption IS NOT NULL")
+
+            if priority is not None:
+                joins.append("JOIN task_note t ON n.id = t.note_id")
+                conditions.append("t.priority = %s")
+                params.append(priority.value)
+
+            full_query = f"{query} {' '.join(joins)} WHERE {' AND '.join(conditions)}"
+
+            cur.execute(full_query, tuple(params))
             notes = cur.fetchall()
+
 
     return Folder(
         **folder_data,
